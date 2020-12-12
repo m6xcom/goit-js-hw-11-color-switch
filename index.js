0efc4c768deea2b3e03be8dd4b2417e247a1542c
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-12-12 12:57:58 +0200

    Добавляет код программы

---
+++ index.js
@@ -0,0 +1,38 @@
+const buttonStart = document.querySelector("[data-action='start']");
+const buttonStop = document.querySelector("[data-action='stop']");
+const body = document.querySelector("body");
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const colorSwitcher = {
+  isActive: false,
+  switcherId: null,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.switcherId = setInterval(() => {
+      let color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+      body.style.backgroundColor = color;
+    }, 1000);
+  },
+  stop() {
+    this.isActive = false;
+    clearInterval(this.switcherId);
+  },
+};
+
+buttonStart.addEventListener("click", colorSwitcher.start.bind(colorSwitcher));
+buttonStop.addEventListener("click", colorSwitcher.stop.bind(colorSwitcher));
